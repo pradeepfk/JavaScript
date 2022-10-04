58accd1f7e8013f97350080635090755cd56d3fe
pradeepfk <114599148+pradeepfk@users.noreply.github.com>
2022-10-04 20:53:39 +0530

    js  file for variables
    js  file for variables

---
+++ cwh_js3.js
@@ -0,0 +1,31 @@
+console.log("hello guys")
+var name= "pradeep";
+var marks = 89
+console.log("print using variable name : " , name ,"marks are : " ,marks)
+/*
+rules for creating a js variables
+1.cannot start with numbers
+2.can start with letter,numbers,underscore(_),$
+3.are case sensitive
+( u can use nut better to not use _ and $)
+*/
+
+
+//  always prefer to use const and let
+
+const myname="pradeepfk" // u can assign only once but u cannot chnage it again
+console.log( "const variable : " ,myname)
+console.warn("u can assign only once but u cannot chnage it again")
+a=232
+{
+    let a=10;
+    console.log("a= " , a) //let is used as local variables
+}
+console.log("a= ", a)
+ /*
+ commanly used case types
+ 1.camelCase
+ 2.kebab-case
+ 3.snake_case
+ 4.PascalCase
+ */
